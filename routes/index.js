523086bf4dc52b896a141bbf4687b8b547866786
Aleksandr Demidenko <sashatro@mail.ru>
2022-10-07 08:35:48 +0300

    feat:Add feedback route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const authRouter = require('./authentication');
 const usersRouter = require('./users');
 const clientsRouter = require('./clients');
+const { createClient } = require('../controllers/clients');
 const auth = require('../middlewares/auth');
 const { signout } = require('../controllers/users');
 
 router.use('/', authRouter);
+router.use('/feedback', createClient);
 router.use('/', auth);
 router.use('/signout', signout);
 router.use('/users', usersRouter);
